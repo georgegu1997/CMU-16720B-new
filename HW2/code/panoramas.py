cd775b489762fe834c23fb1d1d1044f1e8f16375
GU Qiao <georgegu1997@gmail.com>
2019-10-04 20:19:03 -0400

    HW2 everything before blending

--- HW2/code/panoramas.py
+++ HW2/code/panoramas.py
@@ -7,7 +7,7 @@ from BRIEF import briefLite,briefMatch,plotMatches
 
 def imageStitching(im1, im2, H2to1):
     '''
-    Returns a panorama of im1 and im2 using the given homography matrix. 
+    Returns a panorama of im1 and im2 using the given homography matrix.
     Warps img2 into img1 reference frame using the provided warpH() function
 
     INPUT
@@ -17,16 +17,24 @@ def imageStitching(im1, im2, H2to1):
     OUTPUT
         Blends img1 and warped img2 and outputs the panorama image
     '''
-    
+
     #######################################
     # TO DO ...
+    # Let the output to be larger than the input
+    output_shape = (im2.shape[1] * 2, im2.shape[0])
+    warp_im = cv2.warpPerspective(im2, H2to1, output_shape)
+
+    pano_im = np.zeros((output_shape[1], output_shape[0], 3), dtype=np.uint8)
+    pano_im[:im1.shape[0], :im1.shape[1]] = im1
+    # Using elementwise maximum to blend
+    pano_im = np.maximum(warp_im, pano_im)
 
     return pano_im
 
 
 def imageStitching_noClip(im1, im2, H2to1):
     '''
-    Returns a panorama of im1 and im2 using the given homography matrix without clipping. 
+    Returns a panorama of im1 and im2 using the given homography matrix without clipping.
     Warps img2 into img1 reference frame using the provided warpH() function
 
     INPUT
@@ -36,10 +44,10 @@ def imageStitching_noClip(im1, im2, H2to1):
     OUTPUT
         Blends img1 and warped img2 and outputs the panorama image
     '''
-    
+
     ######################################
     # TO DO ...
-    
+
     return pano_im
 
 
@@ -50,15 +58,44 @@ def generatePanaroma(im1, im2):
     INPUT
         im1 and im2 - two images for stitching
     OUTPUT
-        Blends img1 and warped img2 (with no clipping) 
+        Blends img1 and warped img2 (with no clipping)
         and saves the panorama image.
     '''
 
     ######################################
     # TO DO ...
 
-
-if __name__ == '__main__':
+def main():
     im1 = cv2.imread('../data/incline_L.png')
     im2 = cv2.imread('../data/incline_R.png')
-    generatePanaroma(im1, im2)
+    print(im1.shape)
+    locs1, desc1 = briefLite(im1)
+    locs2, desc2 = briefLite(im2)
+    matches = briefMatch(desc1, desc2)
+    # plotMatches(im1,im2,matches,locs1,locs2)
+    H2to1 = ransacH(matches, locs1, locs2, num_iter=5000, tol=2)
+
+    # 6.1
+    pano_im = imageStitching(im1, im2, H2to1)
+    cv2.imwrite('../results/6_1.jpg', pano_im)
+    np.save('../results/q6_1.npy', H2to1)
+    cv2.imshow('panoramas', pano_im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # 6.2 No cliping
+    pano_im = imageStitching_noClip(im1, im2, H2to1)
+    cv2.imwrite('../results/6_2_pan.jpg', pano_im)
+    cv2.imshow('panoramas', pano_im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # 6.3 all together
+    pano_im = generatePanorama(im1, im2)
+    cv2.imwrite('../results/q6_3.jpg', pano_im)
+    cv2.imshow('panoramas', pano_im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
--- HW2/code/planarH.py
+++ HW2/code/planarH.py
@@ -6,25 +6,39 @@ from BRIEF import briefLite, briefMatch
 def computeH(p1, p2):
     '''
     INPUTS
-        p1 and p2 - Each are size (2 x N) matrices of corresponding (x, y)'  
+        p1 and p2 - Each are size (2 x N) matrices of corresponding (x, y)'
                 coordinates between two images
     OUTPUTS
-        H2to1 - a 3 x 3 matrix encoding the homography that best matches the linear 
+        H2to1 - a 3 x 3 matrix encoding the homography that best matches the linear
                 equation
     '''
-    
+
     assert(p1.shape[1]==p2.shape[1])
     assert(p1.shape[0]==2)
     #############################
     # TO DO ...
-    
+    N = p1.shape[1]
+
+    # \lambda p1 = H2to1 p2. Therefore, p1 = x, p2 = u
+    # Construct the A matrix
+    x = np.hstack([p1.T, np.ones((N, 1))])
+    u = np.hstack([p2.T, np.ones((N, 1))])
+    rpart1 = - u * x[:, 0:1]
+    rpart2 = - u * x[:, 1:2]
+    A = np.hstack([u, np.zeros((N, 3)), rpart1, np.zeros((N, 3)), u, rpart2])
+    A = A.reshape([2*N, -1])
+
+    # compute SVD for A
+    u, s, vh = np.linalg.svd(A, full_matrices=True)
+    # Get the last row of vh and reshape to get H
+    H2to1 = vh[-1, :].reshape((3, 3))
     return H2to1
 
 
 def ransacH(matches, locs1, locs2, num_iter=5000, tol=2):
     '''
     Returns the best homography by computing the best set of matches using RANSAC
-    
+
     INPUTS
         locs1 and locs2 - matrices specifying point locations in each of the images
         matches         - matrix specifying matches between these two sets of point locations
@@ -37,9 +51,43 @@ def ransacH(matches, locs1, locs2, num_iter=5000, tol=2):
 
     ###########################
     # TO DO ...
-    
-    return bestH
 
+    bestH = []
+    best_inliers = []
+    sample_space = np.arange(0, matches.shape[0])
+
+    # \lambda p1 = H2to1 p2. Therefore, p1 = x, p2 = u
+    for i in range(num_iter):
+        # Random sample the matches
+        idx = np.random.choice(sample_space, size=4, replace=False)
+        sampled_matches = matches[idx]
+        sampled_locs1 = locs1[sampled_matches[:,0], :2]
+        sampled_locs2 = locs2[sampled_matches[:,1], :2]
+
+        # Compute and apply the transformation matrix H
+        H2to1 = computeH(sampled_locs1.T, sampled_locs2.T)
+        u = locs2[matches[:,1]].T
+        u[2, :] = 1
+        x = locs1[matches[:,0]].T
+        x[2, :] = 1
+        Hu = H2to1.dot(u)
+        # Normalize Hu because of the lambda
+        normalized_Hu = Hu / Hu[2,:]
+        error = ((normalized_Hu-x)[:2, :] ** 2).sum(axis=0)
+
+        # Get the index of inliers
+        inlier_idx = error < tol
+        n_inliers = inlier_idx.sum()
+        inliers = matches[inlier_idx]
+        if n_inliers > len(best_inliers):
+            best_inliers = inliers
+            bestH = H2to1
+
+    print("RANSAC found the best H with %d/%d inliers" % (len(best_inliers), len(matches)))
+    # print("Best H is:")
+    # print(bestH)
+
+    return bestH
 
 if __name__ == '__main__':
     im1 = cv2.imread('../data/model_chickenbroth.jpg')
