a70e1b2663e3b4c30082b827a29c83cbf004b775
GU Qiao <georgegu1997@gmail.com>
2019-10-14 23:44:43 -0400

    Migrating code

--- HW3/code/LucasKanade.py
+++ HW3/code/LucasKanade.py
@@ -2,15 +2,63 @@ import numpy as np
 from scipy.interpolate import RectBivariateSpline
 
 def LucasKanade(It, It1, rect, p0 = np.zeros(2)):
-	# Input: 
-	#	It: template image
-	#	It1: Current image
-	#	rect: Current position of the car
-	#	(top left, bot right coordinates)
-	#	p0: Initial movement vector [dp_x0, dp_y0]
-	# Output:
-	#	p: movement vector [dp_x, dp_y]
-	
+    # Input:
+    #    It: template image
+    #    It1: Current image
+    #    rect: Current position of the car
+    #    (top left, bot right coordinates)
+    #    p0: Initial movement vector [dp_x0, dp_y0]
+    # Output:
+    #    p: movement vector [dp_x, dp_y]
+
     # Put your implementation here
-    p = p0
+    p = p0.copy()
+
+    # Get the interpolation
+    th, tw = It.shape
+    ih, iw = It1.shape
+    interp_It = RectBivariateSpline(np.arange(th), np.arange(tw), It)
+    interp_It1 = RectBivariateSpline(np.arange(ih), np.arange(iw), It1)
+
+    # Create the initial rectangle
+    X = np.arange(rect[0, 0], rect[2, 0]+1, 1)
+    Y = np.arange(rect[1, 0], rect[3, 0]+1, 1)
+
+    while True:
+        # Get the image gradient, Y first because Y indicates row number
+        It1_y_grad = interp_It1(Y+p[1], X+p[0], dx=1) # dx=1 means first derivative along the first input Y
+        It1_x_grad = interp_It1(Y+p[1], X+p[0], dy=1)
+
+        # Construct A and b
+        A = np.hstack([It1_x_grad.reshape(-1, 1), It1_y_grad.reshape(-1, 1)])
+        b = (interp_It(Y, X) - interp_It1(Y+p[1], X+p[0])).reshape(-1, 1)
+
+        # Solve the least square problem
+        dp, _, _, _ = np.linalg.lstsq(A, b, rcond=None)
+
+        # p <- p + dp and the compare
+        old_p = p.copy()
+        p[0] += dp[0, 0]
+        p[1] += dp[1, 0]
+
+        change = np.linalg.norm(dp)**2
+        if change < 0.01:
+            break
+
     return p
+
+class LucasKanadeTracker():
+    def __init__(self, rect, frame=None):
+        self.init_rect = rect
+        self.init_frame = frame
+        self.rect = self.init_rect.copy()
+
+    def update(self, f1, f2):
+        dp = LucasKanade(f1, f2, self.rect)
+        self.rect[0] += dp[0]
+        self.rect[2] += dp[0]
+        self.rect[1] += dp[1]
+        self.rect[3] += dp[1]
+
+    def get_rect(self):
+        return self.rect
--- HW3/code/testCarSequence.py
+++ HW3/code/testCarSequence.py
@@ -4,3 +4,65 @@ from matplotlib import animation
 import matplotlib.patches as patches
 
 # write your script here, we recommend the above libraries for making your animation
+
+from LucasKanade import LucasKanadeTracker
+import cv2
+
+# '''[p1:p3, p0:p2]'''
+INPUT_NPY = "../data/carseq.npy"
+INIT_RECT = [[59], [116], [145], [151]]
+IMG_SAVE_NAME = "carseqrects"
+RESULTS_SAVE_PATH = "../results/carseqrects.npy"
+
+def runTracker(frames, init_rect, trackers, img_save_name, results_save_path=None, to_show=False, save_frame=[0, 99, 199, 299, 399]):
+    COLORS = [[0,255,255], [0,255,0]]
+    results = []
+
+    for i in range(frames.shape[2] - 1):
+        It = frames[:,:,i]
+        It1 = frames[:,:,i+1]
+
+        # Convert gray image to RGB one
+        if to_show or i in save_frame:
+            show = cv2.cvtColor((It1*255).astype(np.uint8),cv2.COLOR_GRAY2RGB)
+
+        for j, tracker in enumerate(trackers):
+            tracker.update(It, It1)
+            rect = tracker.get_rect()
+
+            # Only save the results from the last tracker
+            if j == len(trackers) - 1 :
+                results.append(rect)
+
+            # Draw the rectangle for each tracker
+            if to_show or i in save_frame:
+                cv2.rectangle(show, (rect[0], rect[1]), (rect[2], rect[3]), COLORS[j], 2)
+
+        if to_show:
+            cv2.imshow("", show)
+            cv2.waitKey(0) # press any key to exit
+        if i in save_frame:
+            print("Saving ../results/%s_%d.png" % (img_save_name, i))
+            cv2.imwrite("../results/%s_%d.png" % (img_save_name, i), show)
+
+    if to_show:
+        cv2.destroyAllWindows()
+
+    results = np.array(results)
+    if not results_save_path is None:
+        print("Saveing", results_save_path)
+        np.save(results_save_path, results)
+
+    return results
+
+def main():
+    frames = np.load(INPUT_NPY)
+    frame0 = frames[:,:,0]
+    rect0 = np.array(INIT_RECT, dtype=float)
+
+    trackers = []
+    trackers.append(LucasKanadeTracker(rect0))
+    results = runTracker(frames, INIT_RECT, trackers, IMG_SAVE_NAME, RESULTS_SAVE_PATH, to_show=True)
+
+if __name__ == '__main__':
+    main()
