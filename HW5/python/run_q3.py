fb7284592d62cd35c3e6c9aa161044f94b23948d
GU Qiao <georgegu1997@gmail.com>
2019-11-13 17:53:56 -0500

    Q3.1.2 done

--- HW5/python/run_q3.py
+++ HW5/python/run_q3.py
@@ -4,9 +4,12 @@ from nn import *
 
 train_data = scipy.io.loadmat('../data/nist36_train.mat')
 valid_data = scipy.io.loadmat('../data/nist36_valid.mat')
+test_data = scipy.io.loadmat('../data/nist36_test.mat')
+
 
 train_x, train_y = train_data['train_data'], train_data['train_labels']
 valid_x, valid_y = valid_data['valid_data'], valid_data['valid_labels']
+test_x, test_y = test_data['test_data'], test_data['test_labels']
 
 max_iters = 50
 # pick a batch size, learning rate
@@ -23,110 +26,130 @@ batch_size = 50
 learning_rate = 2e-3
 ##########################
 
-batches = get_random_batches(train_x,train_y,batch_size)
-batch_num = len(batches)
-
 params = {}
 
-# initialize layers here
-##########################
-##### your code here #####
-initialize_weights(input_size, hidden_size, params, "layer1")
-initialize_weights(hidden_size, output_size, params, "output")
-epoch_list = []
-train_acc_list = []
-valid_acc_list = []
-train_loss_list = []
-valid_loss_list = []
-##########################
+def trainAndProgress(learning_rate = 2e-3):
+    global params
+    np.random.seed(2019)
+    batches = get_random_batches(train_x,train_y,batch_size)
+    batch_num = len(batches)
 
+    params = {}
 
-# with default settings, you should get loss < 150 and accuracy > 80%
-for itr in range(max_iters):
-    total_loss = 0
-    total_acc = 0
-    for xb,yb in batches:
-        # training loop can be exactly the same as q2!
-        ##########################
-        ##### your code here #####
-        # forward
-        h1 = forward(xb,params,'layer1') # First layer
-        probs = forward(h1,params,'output',softmax) # Second layer
-        # loss
-        loss, acc = compute_loss_and_acc(yb, probs)
-        # be sure to add loss and accuracy to epoch totals
-        total_loss += loss
-        total_acc += acc / batch_num
+    # initialize layers here
+    ##########################
+    ##### your code here #####
+    initialize_weights(input_size, hidden_size, params, "layer1")
+    initialize_weights(hidden_size, output_size, params, "output")
+    epoch_list = []
+    train_acc_list = []
+    valid_acc_list = []
+    train_loss_list = []
+    valid_loss_list = []
+    ##########################
 
-        # backward
-        delta1 = probs.copy()
-        delta1[np.arange(probs.shape[0]),yb.argmax(axis=1)] -= 1
-        delta2 = backwards(delta1,params,'output',linear_deriv)
-        backwards(delta2,params,'layer1',sigmoid_deriv)
+    # with default settings, you should get loss < 150 and accuracy > 80%
+    for itr in range(max_iters):
+        total_loss = 0
+        total_acc = 0
+        for xb,yb in batches:
+            # training loop can be exactly the same as q2!
+            ##########################
+            ##### your code here #####
+            # forward
+            h1 = forward(xb,params,'layer1') # First layer
+            probs = forward(h1,params,'output',softmax) # Second layer
+            # loss
+            loss, acc = compute_loss_and_acc(yb, probs)
+            # be sure to add loss and accuracy to epoch totals
+            total_loss += loss
+            total_acc += acc / batch_num
 
-        # apply gradient
-        for k,v in sorted(list(params.items())):
-            if 'grad' in k:
-                name = k.split('_')[1]
-                # print(np.linalg.norm(learning_rate * v))
-                params[name] -= learning_rate * v
-        ##########################
+            # backward
+            delta1 = probs.copy()
+            delta1[np.arange(probs.shape[0]),yb.argmax(axis=1)] -= 1
+            delta2 = backwards(delta1,params,'output',linear_deriv)
+            backwards(delta2,params,'layer1',sigmoid_deriv)
 
-    if itr % 2 == 0:
-        print("itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(itr,total_loss,total_acc))
-        ##########################
-        ##### your code here #####
-        epoch_list.append(itr+1)
-        train_acc_list.append(total_acc)
-        train_loss_list.append(total_loss)
+            # apply gradient
+            for k,v in sorted(list(params.items())):
+                if 'grad' in k:
+                    name = k.split('_')[1]
+                    # print(np.linalg.norm(learning_rate * v))
+                    params[name] -= learning_rate * v
+            ##########################
 
-        # validation forward
-        h1 = forward(valid_x,params,'layer1') # First layer
-        probs = forward(h1,params,'output',softmax) # Second layer
-        valid_loss, valid_acc = compute_loss_and_acc(valid_y, probs) # Loss
-        valid_acc_list.append(valid_acc)
-        valid_loss_list.append(valid_loss)
-        ##########################
+        if itr % 2 == 0:
+            print("itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(itr,total_loss,total_acc))
+            ##########################
+            ##### your code here #####
+            epoch_list.append(itr+1)
+            train_acc_list.append(total_acc)
+            train_loss_list.append(total_loss)
 
-# run on validation set and report accuracy! should be above 75%
-valid_acc = None
-##########################
-##### your code here #####
-# Final training forward
-h1 = forward(train_x,params,'layer1') # First layer
-probs = forward(h1,params,'output',softmax) # Second layer
-loss, acc = compute_loss_and_acc(train_y, probs) # loss
-print("Training final: \t loss: {:.2f} \t acc : {:.2f}".format(loss,acc))
+            # validation forward
+            h1 = forward(valid_x,params,'layer1') # First layer
+            probs = forward(h1,params,'output',softmax) # Second layer
+            valid_loss, valid_acc = compute_loss_and_acc(valid_y, probs) # Loss
+            valid_acc_list.append(valid_acc)
+            valid_loss_list.append(valid_loss)
+            ##########################
 
-# Validation forward
-h1 = forward(valid_x,params,'layer1') # First layer
-probs = forward(h1,params,'output',softmax) # Second layer
-valid_loss, valid_acc = compute_loss_and_acc(valid_y, probs) # loss
-print('Validation accuracy: ',valid_acc)
+    # run on validation set and report accuracy! should be above 75%
+    valid_acc = None
+    ##########################
+    ##### your code here #####
+    # Final training forward
+    h1 = forward(train_x,params,'layer1') # First layer
+    probs = forward(h1,params,'output',softmax) # Second layer
+    loss, acc = compute_loss_and_acc(train_y, probs) # loss
+    print("Training final: \t loss: {:.2f} \t acc : {:.2f}".format(loss,acc))
 
-# Visualization of the training progress
-def visualizaProgress():
-    import matplotlib.pyplot as plt
-    plt.subplot(211)
-    plt.plot(epoch_list, train_acc_list, label="Training")
-    plt.plot(epoch_list, valid_acc_list, label="Validation")
-    plt.legend()
-    plt.xlabel("Epoch")
-    plt.ylabel("Accuracy")
+    # Validation forward
+    h1 = forward(valid_x,params,'layer1') # First layer
+    probs = forward(h1,params,'output',softmax) # Second layer
+    valid_loss, valid_acc = compute_loss_and_acc(valid_y, probs) # loss
+    print('Validation final: ',valid_acc)
 
-    plt.subplot(212)
-    plt.plot(epoch_list, train_loss_list, label="Training")
-    plt.plot(epoch_list, valid_loss_list, label="Validation")
-    plt.legend()
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
+    # Test forward
+    h1 = forward(test_x,params,'layer1') # First layer
+    probs = forward(h1,params,'output',softmax) # Second layer
+    test_loss, test_acc = compute_loss_and_acc(test_y, probs) # loss
+    print('Testing final: ',test_acc)
 
-    plt.show()
+    # Visualization of the training progress
+    def visualizaProgress():
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(8, 12))
+        plt.subplot(211)
+        plt.title("Learning Rate: %.2E" % learning_rate)
+        plt.plot(epoch_list, train_acc_list, label="Training")
+        plt.plot(epoch_list, valid_acc_list, label="Validation")
+        plt.ylim(0,1)
+        plt.legend()
+        plt.xlabel("Epoch")
+        plt.ylabel("Accuracy")
 
-# visualizaProgress()
-##########################
+        plt.subplot(212)
+        plt.plot(epoch_list, train_loss_list, label="Training")
+        plt.plot(epoch_list, valid_loss_list, label="Validation")
+        plt.ylim(0,40000)
+        plt.legend()
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
 
-print('Validation accuracy: ',valid_acc)
+        # plt.show()
+        plt.savefig('../results/q3_1_2_%.0E.png' % learning_rate)
+        plt.clf()
+        plt.close()
+
+    visualizaProgress()
+    ##########################
+
+trainAndProgress(learning_rate/10.0)
+trainAndProgress(learning_rate*10.0)
+trainAndProgress(learning_rate)
+
 if False: # view the data
     for crop in xb:
         import matplotlib.pyplot as plt
@@ -144,7 +167,6 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 # visualize weights here
 ##########################
 ##### your code here #####
-
 ##########################
 
 # Q3.1.4
